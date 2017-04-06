ff7f74184e43aa04ba4270cd887cbaae4ec5f344
Louis <louis.delarue@isen-lille.fr>
2017-04-06 10:27:53 +0200

    Adding UnitManager

--- FirstStock/src/Models/Unit.java
+++ FirstStock/src/Models/Unit.java
@@ -5,16 +5,44 @@
  */
 package Models;
 
+import java.io.Serializable;
+
 /**
  *
  * @author louis
  */
-public enum Unit {
+public class Unit implements Serializable{
     /* Penser à implenter une sérialisation de l'enum afin de rajouter des 
         unités par l'utilisateur
     */
-    kg,
-    g,
-    L,
-    mL
+    private String unit;
+    private int power;
+    
+    public Unit(String u, int p){
+        this.setUnit(u);
+        this.setPower(p);
+    }
+    
+    /* Getters & setters */
+
+    public String getUnit() {
+        return unit;
+    }
+
+    public void setUnit(String unit) {
+        this.unit = unit;
+    }
+
+    public int getPower() {
+        return power;
+    }
+
+    public void setPower(int power) {
+        this.power = power;
+    }
+    
+    @Override
+    public String toString(){
+        return "Unit : "+unit+" with 10^"+power;
+    }
 }
---
+++ FirstStock/src/Models/UnitManager.java
@@ -0,0 +1,115 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Models;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Set;
+/**
+ *
+ * @author louis
+ */
+public class UnitManager {
+    private String fileName;
+    private Map<String, Integer> units;
+    
+    UnitManager(String f){
+        fileName = f;
+        units = new LinkedHashMap<>();
+        this.write();
+    }
+    
+    public void clear(){
+        this.units.clear();
+        this.write();
+    }
+    
+    public void add(String s, Integer i){
+        units.put(s,i);
+    }
+    
+    public void write(){
+        // Ecriture
+        ObjectOutputStream dataOut;
+        try{
+            dataOut = new ObjectOutputStream(
+                        new BufferedOutputStream(
+                            new FileOutputStream(
+                                new File(this.fileName))));
+            // Sauvegarde des unités
+            dataOut.writeObject(units);
+            // Fermeture du flux
+            dataOut.close();
+        } catch (FileNotFoundException e) {
+            // Si le fichier est inexistant
+            e.printStackTrace();
+        } catch (IOException e) {
+            // Si la lecture est impossible
+          e.printStackTrace();
+        }
+    }
+    
+    public void read(){
+        this.units.clear();
+
+        try{
+            // Affichage
+            ObjectInputStream dataIn;
+
+            dataIn = new ObjectInputStream(
+                        new BufferedInputStream(
+                          new FileInputStream(
+                            new File(this.fileName))));
+            
+            // Lecture des données
+            try{
+                // Récupération du nombre de scores à lire               
+                units = (Map<String, Integer>)dataIn.readObject();
+            }
+            catch (ClassNotFoundException e){
+                // Si Il n'y a pas de class à récupérer
+                e.printStackTrace();
+            }
+            // Fermeture du flux
+            dataIn.close();        
+        }
+         catch (FileNotFoundException e) {
+            // Si le fichier est inexistant
+            e.printStackTrace();
+        } catch (IOException e) {
+            // Si la lecture est impossible
+            e.printStackTrace();
+        }
+    }
+    
+    public Map<String, Integer> getUnits() {
+        return units;
+    }
+    
+    @Override
+    public String toString(){
+        Set<Entry<String, Integer>> setUnits = units.entrySet();
+        Iterator<Entry<String, Integer>> it = setUnits.iterator();
+        String str = "";
+        while(it.hasNext()){
+            Entry<String, Integer> e = it.next();
+            str += e.toString()+"\n";
+        }
+        return str;
+    }
+}
